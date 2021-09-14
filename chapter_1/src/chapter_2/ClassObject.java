97e41c4bf673cc63ce09298b01ed75caabcd8e0f
kurniawan86 <90589478+kurniawan86@users.noreply.github.com>
2021-09-15 05:38:21 +0700

    piye carane jal

--- chapter_1/src/chapter_2/ClassObject.java
+++ chapter_1/src/chapter_2/ClassObject.java
@@ -1,11 +1,20 @@
 package chapter_2;
 
+class ClassCoba
+{
+    //attributes:
+    //data_type var_name1;
+    //data_type var_name2;
+
+    //methods:
+    public void method_name1( )
+    {
+
+    }
+}
 public class ClassObject
 {
     public static void main(String[]args)
     {
-        System.out.println("hello git");
-        System.out.println("ra isok maneh");
-        System.out.println("kepriye carane jal");
     }
 }
