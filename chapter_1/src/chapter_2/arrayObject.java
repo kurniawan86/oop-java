947f2802cfb52cd271cf4ecb04a666e909cc7ecf
kurniawan86 <90589478+kurniawan86@users.noreply.github.com>
2021-10-13 11:45:27 +0700

    object array 11.45

--- chapter_1/src/chapter_2/arrayObject.java
+++ chapter_1/src/chapter_2/arrayObject.java
@@ -21,13 +21,14 @@ public class arrayObject
 {
     public static void main(String[]args)
     {
-        segitiga [] triagle = new segitiga[2];
-        triagle[0] = new segitiga(3,2);
+        segitiga [] triangle;
+        triangle = new segitiga[2];
+        triangle[0] = new segitiga(3,2);
         System.out.println("luas Segitiga 1:"+
-                triagle[0].CalculateArea());
-        triagle[1] = new segitiga(10,5);
+                triangle[0].CalculateArea());
+        triangle[1] = new segitiga(10,5);
         System.out.println("luas Segitiga 2:"+
-                triagle[1].CalculateArea());
+                triangle[1].CalculateArea());
     }
 }
 
